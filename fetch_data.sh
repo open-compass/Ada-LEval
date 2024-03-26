225d068472ec7259044bdbd1ad52110baf579cdd
kennymckormick <dhd@pku.edu.cn>
2024-03-26 20:51:27 +0800

    update

---
+++ fetch_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in 1k 2k 4k 6k 8k 16k 32k 64k 128k
+do
+wget http://opencompass.openxlab.space/utils/AdaLEval/stackselect_$i.json -O data/stackselect_$i.json;
+wget http://opencompass.openxlab.space/utils/AdaLEval/textsort_$i.json -O data/textsort_$i.json;
+done
